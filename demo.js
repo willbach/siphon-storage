04ef53fd5b8cdcea991af5f2a0652a609aef1dfd
Will Galebach <willbach@gmail.com>
2016-12-19 19:25:43 -0800

    added demo server

--- demo.js
+++ demo.js
@@ -22,11 +22,20 @@ app.get('/temperatures', (req, res) => {
   Temperatures.findAll()
   .then( (data) => {
     console.log('here is the data', data.length);
+    let interval = 10;
+    let bunch = 200;
+
+    for(i = 0; i < data.length; i += bunch) {
+      let temps = [];
+      for(j = 0; j < bunch, data[j]; j++) {
+        temps.push(data[i+j]);
+      }
+      console.log('bunch of temps for ya', temps.length);
+      io.emit('temps', temps);
+    }
+
   })
   res.end();
-  
-
-  io.emit('game update', newState);
 
 });
 
