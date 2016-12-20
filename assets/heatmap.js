703c4187443e750e43f6d5f2175146bc6ad0e882
Will Galebach <willbach@gmail.com>
2016-12-20 09:57:26 -0800

    lets do this

--- assets/heatmap.js
+++ assets/heatmap.js
@@ -447,20 +447,21 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
         shadowCtx.drawImage(tpl, rectX, rectY);
 
         // update renderBoundaries
-        if (rectX < this._renderBoundaries[0]) {
-            this._renderBoundaries[0] = rectX;
-          }
-          if (rectY < this._renderBoundaries[1]) {
-            this._renderBoundaries[1] = rectY;
-          }
-          if (rectX + 2*radius > this._renderBoundaries[2]) {
-            this._renderBoundaries[2] = rectX + 2*radius;
-          }
-          if (rectY + 2*radius > this._renderBoundaries[3]) {
-            this._renderBoundaries[3] = rectY + 2*radius;
-          }
+        //modified by Will G
+      //   if (rectX < this._renderBoundaries[0]) {
+      //       this._renderBoundaries[0] = rectX;
+      //     }
+      //     if (rectY < this._renderBoundaries[1]) {
+      //       this._renderBoundaries[1] = rectY;
+      //     }
+      //     if (rectX + 2*radius > this._renderBoundaries[2]) {
+      //       this._renderBoundaries[2] = rectX + 2*radius;
+      //     }
+      //     if (rectY + 2*radius > this._renderBoundaries[3]) {
+      //       this._renderBoundaries[3] = rectY + 2*radius;
+      //     }
 
-      }
+      // }
     },
     _colorize: function() {
       var x = this._renderBoundaries[0];
