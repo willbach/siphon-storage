0d17709cb596a5e08011d5197b5703b25ad393f4
Will Galebach <willbach@gmail.com>
2016-12-19 18:57:50 -0800

    added demo server

---
+++ demo.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const dataController = require('./dataController');
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, './')));
+app.use(bodyParser.json(), bodyParser.urlencoded({extended: true}));
+
+app.post('/stackoverflow', dataController.stackOverflow);
+
+app.post('/temperatures', dataController.temperatures);
+
+app.get('/temperatures', (req, res) => {
+  let newState = req.body;
+  res.end();
+  console.log('game update', newState);
+  io.emit('game update', newState);
+  
+});
+
+io.on('connection', (client) => {
+  console.log('client connected');
+  
+  client.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+});
+
+server.listen(3000, () => console.log('listening on port 3000'));
