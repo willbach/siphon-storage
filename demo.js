c3c9d861b5af4373b0884a65fcf38a6ecbbb37e3
Will Galebach <willbach@gmail.com>
2016-12-19 19:02:35 -0800

    added demo server

--- demo.js
+++ demo.js
@@ -31,4 +31,4 @@ io.on('connection', (client) => {
   });
 });
 
-server.listen(3000, () => console.log('listening on port 3000'));
+server.listen(3000, () => console.log(`listening on port ${PORT}`));
