a2185e12edf675c5898bba0a973bb9ac086b5748
Will Galebach <willbach@gmail.com>
2016-12-19 20:19:59 -0800

    lets hope this works

--- demo/demo.js
+++ demo/demo.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const dataController = require('./dataController');
-const Temperatures = require('./tempModel');
+const dataController = require('../dataController');
+const Temperatures = require('../tempModel');
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
