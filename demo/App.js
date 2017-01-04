ba99cc40b2fa59f93febfde72b3d62fb44c01293
Will Galebach <willbach@gmail.com>
2017-01-03 22:29:58 -0800

    responsive

--- demo/App.js
+++ demo/App.js
@@ -30,14 +30,14 @@ class App extends React.Component {
     let errHeight = (this.state.err/20).toString()+"px";
 
     return (
-      <div style={{height: "56.5%", width: "18.32%", flexDirection: 'row', display: 'flex'}}>
+      <div style={{height: "434px", width: "18.32%", flexDirection: 'row', display: 'flex'}}>
 
-        <div style={{height:"100%", width: "11%", position: "relative"}}>
+        <div style={{height:"434px", width: "11%", position: "relative"}}>
           <div style={{backgroundColor: 'green', color: 'white', fontSize: "20px", width: "4.4%", marginLeft: "3.66%", height: sucHeight, position: "absolute", bottom: "38px"}}>{this.state.suc}</div>
           <div style={{width: "11%", position: "absolute", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderRightWidth: '0px'}}>Successes</div>
         </div>
 
-        <div style={{height:"100%", width: "7.32%", position: "relative"}}>
+        <div style={{height:"434px", width: "7.32%", position: "relative"}}>
           <div style={{backgroundColor: 'red', color: 'white', width: "4.4%", fontSize: "20px", marginLeft: "1.46%", height: errHeight, position: "absolute", bottom: "38px"}}>{this.state.err}</div>
           <div style={{width: "7.32%", position: "absolute", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderLeftWidth: '0px'}}>Errors</div>
         </div>
