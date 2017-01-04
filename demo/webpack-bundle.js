bf5f576689298cad9f480eb9985b857e5eb2881c
Will Galebach <willbach@gmail.com>
2017-01-03 22:16:34 -0800

    responsive

--- demo/webpack-bundle.js
+++ demo/webpack-bundle.js
@@ -21548,12 +21548,12 @@
 	          { style: { height: "31.8%", width: "11%", position: "relative" } },
 	          _react2.default.createElement(
 	            'div',
-	            { style: { backgroundColor: 'green', color: 'white', fontSize: "20px", width: "4.4%", marginLeft: "3.66%", height: sucHeight, position: "absolute", bottom: "38px" } },
+	            { style: { backgroundColor: 'green', color: 'white', fontSize: "20px", width: "4.4%", marginLeft: "3.66%", height: sucHeight, bottom: "38px" } },
 	            this.state.suc
 	          ),
 	          _react2.default.createElement(
 	            'div',
-	            { style: { width: "11%", position: "absolute", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderRightWidth: '0px' } },
+	            { style: { width: "11%", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderRightWidth: '0px' } },
 	            'Successes'
 	          )
 	        ),
@@ -21562,12 +21562,12 @@
 	          { style: { height: "31.8%", width: "7.32%", position: "relative" } },
 	          _react2.default.createElement(
 	            'div',
-	            { style: { backgroundColor: 'red', color: 'white', width: "4.4%", fontSize: "20px", marginLeft: "1.46%", height: errHeight, position: "absolute", bottom: "38px" } },
+	            { style: { backgroundColor: 'red', color: 'white', width: "4.4%", fontSize: "20px", marginLeft: "1.46%", height: errHeight, bottom: "38px" } },
 	            this.state.err
 	          ),
 	          _react2.default.createElement(
 	            'div',
-	            { style: { width: "7.32%", position: "absolute", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderLeftWidth: '0px' } },
+	            { style: { width: "7.32%", fontSize: "20px", bottom: "0px", color: 'white', backgroundColor: '#b20000', padding: '5px 0px', border: '2px solid black', borderLeftWidth: '0px' } },
 	            'Errors'
 	          )
 	        )
